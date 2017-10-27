af1fcbd3f2cd90784ebcecbf2f346a589fd026da
JérémyT <talbot.jtalbot@gmail.com>
2017-10-27 15:20:23 +0200

    Add files via upload

---
+++ geronimo.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  ven. 27 oct. 2017 à 14:45
+-- Version du serveur :  10.1.26-MariaDB
+-- Version de PHP :  7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `geronimo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+CREATE TABLE `commentaires` (
+  `IdComments` int(20) NOT NULL,
+  `Pseudo` varchar(25) DEFAULT NULL,
+  `Commentaires` varchar(25) DEFAULT NULL,
+  `DateCommentaire` datetime DEFAULT NULL,
+  `IdArticle` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+CREATE TABLE `likes` (
+  `idLike` int(100) NOT NULL,
+  `pseudo` varchar(250) NOT NULL,
+  `idarticle` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `news`
+--
+
+CREATE TABLE `news` (
+  `idnews` int(20) NOT NULL,
+  `photo` varchar(250) NOT NULL,
+  `text` text NOT NULL,
+  `likes` int(100) NOT NULL,
+  `comments` int(100) NOT NULL,
+  `title` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `news`
+--
+
+INSERT INTO `news` (`idnews`, `photo`, `text`, `likes`, `comments`, `title`) VALUES
+(1, 'image1', 'La deuxième saison de Stranger Things approche à grands pas. Netflix dévoile donc...', 20, 10, 'Stranger Things 2 Amazing New Posters'),
+(2, 'image2', 'Le photographe Akos Major armé de son Mamiya7, chargé de pellicules Kodak Portra 400...', 65, 22, 'Beautiful Series Across the USA'),
+(3, 'image3', 'Dans cette série de tableaux, l’artiste Dries Ketels se penche sur la célèbre...', 30, 15, 'Game of Thrones Crystal Made Artworks by Dries Ketels'),
+(4, 'image4', 'Passionné par l’art et le design, Al Power est un illustrateur irlandais installé à...', 16, 10, 'Colorful Doors of Dublin Illustrations'),
+(5, 'image5', 'Alexander Semenov est un photographe marin et biologiste qui est en train de conduire...', 32, 14, 'Alexander Semenov Expedition to Shoot...'),
+(6, 'image1', 'Edoardo Tresoldi explore le concept de « fantôme » à travers ses incroyables...', 25, 0, 'Ghostly Floating Installation by Edoardo...');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`IdComments`),
+  ADD KEY `IdArticle` (`IdArticle`);
+
+--
+-- Index pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`idLike`),
+  ADD KEY `idarticle` (`idarticle`);
+
+--
+-- Index pour la table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`idnews`),
+  ADD KEY `comments` (`comments`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `IdComments` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`IdArticle`) REFERENCES `news` (`idnews`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`idarticle`) REFERENCES `news` (`idnews`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,79 @@
+﻿<?php
+$pdo = new PDO('mysql:host=localhost;dbname=geronimo', 'root', '', array(
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+));
+
+$resultat = $pdo -> query("SELECT * FROM news");
+?>
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+	<meta name="viewport" content="width= device-width, initial-scale=1.0">
+    <title>Test Jeremy Talbot - Geronimo Agency </title>
+	<link href="styles.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+     
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">dgsn.media</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Pop culture</a></li>
+		<li><a href="#">Design</a></li>
+        <li><a href="#">Photography</a></li>
+		<li><a href="#">About us</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+		
+      <header>
+	  <img src="img/header.png">
+		<h1>Discover smart news
+			<br>for smart people.</h1>
+	  </header>
+     <div class="latest col-12"><span>Latest news</span></div>
+	 <div>
+		<?php
+	 
+		while($news = $resultat -> fetch(PDO::FETCH_ASSOC)){
+		?>
+		<div class=" news col-4">
+			<div><img src='img/<?=$news['photo']?>.png'></div>
+			<div class="title"><?= $news['title']?></div>
+			<div class="text"><?= $news['text']?></div>
+			<div class="likes col-6"><i class="fa fa-heart" aria-hidden="true"></i><?= $news['likes']?> likes</div>
+			<div class="comments col-6"><i class="fa fa-comment" aria-hidden="true"></i><?= $news['comments']?> comments</div>
+		</div>
+		<?php
+		}?>
+	</div>
+	<div class="clear"></div>
+	<footer class="col-12">
+	
+		<ul>
+			<li><a href="">Instagram</a></li>
+			<li><a href="">Facebook</a></li>
+			<li><a href="">Twitter</a></li>
+			<li><a href="">Terms of service and privacy</a></li>
+		</ul>
+		
+	</footer>
+</body>
+</html>
